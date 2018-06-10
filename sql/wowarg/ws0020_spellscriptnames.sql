2e0f5b75e4f9120367cceca100fcede13361694c
Malganys <gatuso1999@yahoo.com.ar>
2018-06-10 15:51:46 -0300

    Acomodamiento de archivos y cambios en el size de las columnas "RewardCurrencyCount" de TINYINT a SMALLINT.

--- sql/wowarg/ws0020_spellscriptnames.sql
+++
@@ -1,7 +0,0 @@
-DELETE FROM spell_script_names where spell_id = 85673 and scriptname = 'spell_pal_word_of_glory';
-INSERT INTO spell_script_names (spell_id, scriptname) VALUES
-(85673, "spell_pal_word_of_glory");
-
-DELETE FROM spell_script_names where spell_id = 8936;
-INSERT INTO spell_script_names (spell_id, scriptnames) VALUES
-(8936, 'spell_dru_emp_touch');
